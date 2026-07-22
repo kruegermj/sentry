d1b812b9455d28092d7223f4e1e1ac5f92b51bce
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-07-22 10:08:36 -0700

    feat(seer): add autofix embed widget

--- static/app/components/seer/markdown/embeds/index.ts
+++ static/app/components/seer/markdown/embeds/index.ts
@@ -1,8 +1,9 @@
+import {Autofix} from './components/autofix';
 import {Docs} from './components/docs';
 import {Timestamp} from './components/timestamp';
 import {SeerEmbedRegistry} from './registry';
 
-const embeds = [Docs, Timestamp];
+const embeds = [Docs, Timestamp, Autofix];
 for (const embed of embeds) {
   SeerEmbedRegistry.register(embed.displayName, embed);
 }
